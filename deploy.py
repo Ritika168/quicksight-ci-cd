e6d42d8a31ad4900a5fc3fcc59cce04461335bb6
Ritika <ritikabhadani23@gmail.com>
2026-04-19 11:02:21 +0530

    Update deploy script

--- deploy.py
+++ deploy.py
@@ -47,7 +47,7 @@ def create_template():
     qs.create_template(
         AwsAccountId=ACCOUNT_ID,
         TemplateId=TEMPLATE_ID,
-        Name="Ritika Template",
+        Name="Ritika Template 2",
         SourceEntity={
             "SourceAnalysis": {
                 "Arn": f"arn:aws:quicksight:{REGION}:{ACCOUNT_ID}:analysis/{SOURCE_ANALYSIS_ID}",
